2b06be5d0bbf0be77db9867d404a3619eda46b53
Mariia Korol <marie.korol1991@gmail.com>
2023-06-28 23:57:29 +0300

    fixed

--- js/index.js
+++ js/index.js
@@ -12,6 +12,30 @@ liItems.forEach((li) => {
   });
 });
 
+// Popup
+const buttons = document.querySelectorAll('.btn--color');
+const popup = document.querySelector('.popup');
+const popupClose = document.querySelector('.popup_close');
+buttons.forEach((button) =>
+  button.addEventListener('click', (event) => {
+    event.stopPropagation();
+    popup.classList.toggle('popup_open');
+    document.body.style.overflow = 'hidden';
+  }),
+);
+popupClose.addEventListener('click', () => {
+  popup.classList.remove('popup_open');
+  document.body.style.overflow = '';
+});
+
+document.addEventListener('click', (event) => {
+  const target = event.target;
+  if (!popup.contains(target) && target !== popup) {
+    popup.classList.remove('popup_open');
+    document.body.style.overflow = '';
+  }
+});
+
 // Button to top
 const scrollBtnToTop = document.querySelector('.btn--toTop');
 scrollBtnToTop.addEventListener('click', () => {
