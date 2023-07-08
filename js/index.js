08a85691c2c7d261641d125ff6ecdf6207f1fdcb
Mariia Korol <marie.korol1991@gmail.com>
2023-07-08 13:44:31 +0300

    fixed

--- js/index.js
+++ js/index.js
@@ -87,7 +87,7 @@ popupClose.forEach((button) =>
 );
 document.addEventListener('click', (event) => {
   const target = event.target;
-  if (!target.closest('.popup__wrapper') && !target.closest('.popup_close') && !target.closest('.iti__flag-container')) {
+  if (!target.closest('.popup__wrapper') && !target.closest('.popup_close') && !target.closest('.iti__country-list')) {
     popup.classList.remove('show');
     document.body.style.overflow = '';
   }
